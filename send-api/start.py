844c785f45367f76c8637f64ca3861d123cdf5d9
Felixim0 <aldam.gatesfelix@gmail.com>
2023-03-29 04:03:00 +0100

    Moved lcd helpers into own file, renamed gpio helpers more appropriately

--- send-api/start.py
+++ send-api/start.py
@@ -1,7 +1,11 @@
 # import required libraries
-from helpers import lcdHelpers
+from helpers import gpio_helpers
+from helpers import lcd_helpers as lcd
 import Adafruit_DHT
 from time import sleep
 
 # Setup Pins
-lcdHelpers.setupPins()
+gpio_helpers.setupPins()
+
+# Run test lcd
+lcd.setup_lcd()
