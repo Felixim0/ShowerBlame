034c2e182bf374eaafc24dc1bf0578e675d798c4
Felixim0 <aldam.gatesfelix@gmail.com>
2023-03-29 02:36:55 +0100

    Added loop to temp sensor

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Stop it. Stop using water while I`m in the shower. No I don't care that you need
 The Idea: Have a button that when pressed, shows a red light at all other water-usage points in the house, showing that the shower is in use and that using water will cause that person to be burned to a crisp or frozen into Elsa. The idea is by making it obvious the shower is running, people can not use the sink for 5 more minutes.
 
 ## Instructions on how to run each Send/Receive api are in their respective directories
+
+https://www.digikey.co.uk/en/maker/blogs/2021/how-to-connect-a-keypad-to-a-raspberry-pi
--- send-api/exampleTempHumidity.py
+++ send-api/exampleTempHumidity.py
@@ -1,15 +1,19 @@
 import Adafruit_DHT
+from time import sleep
 
 # Set sensor type and BCM GPIO number
 sensor = Adafruit_DHT.DHT11
 gpio = 26
 
-# Attempt to get a reading from the sensor
-humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+while True:
+    sleep(1)
+    
+    # Attempt to get a reading from the sensor
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
 
-# Check if reading was successful
-if humidity is not None and temperature is not None:
-    print('Temperature: {0:0.1f}°C'.format(temperature))
-    print('Humidity: {0:0.1f}%'.format(humidity))
-else:
-    print('Failed to get reading. Try again!')
+    # Check if reading was successful
+    if humidity is not None and temperature is not None:
+        print('Temperature: {0:0.1f}°C'.format(temperature))
+        print('Humidity: {0:0.1f}%'.format(humidity))
+    else:
+        print('Failed to get reading. Try again!')
