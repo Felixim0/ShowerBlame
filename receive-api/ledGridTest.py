331bf92570126faf694d7ff6a4106c793434f737
Felixim0 <aldam.gatesfelix@gmail.com>
2023-03-29 09:40:59 +0100

    different etst code

--- receive-api/ledGridTest.py
+++ receive-api/ledGridTest.py
@@ -1,4 +1,16 @@
-from luma.led_matrix.device import max7219 as led
+from luma.core.interface.serial import spi
+from luma.core.render import canvas
+from luma.led_matrix.device import max7219
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT
 
-device = led.matrix(cascaded = 3)
-device.show_message("Hello world!")
+# Initialize the SPI interface
+serial = spi(port=0, device=0, gpio=None)
+
+# Initialize the max7219 device with the serial interface
+device = max7219(serial, cascaded=3, block_orientation=-90)
+
+# Display the message
+show_message(device, "Hello world!", fill="white", font=proportional(CP437_FONT))
+
