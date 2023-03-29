27038796b316b11cd63cd634c2aa35deff2b869a
Felixim0 <aldam.gatesfelix@gmail.com>
2023-03-29 04:15:49 +0100

    Added value debugging

--- send-api/helpers/gpio_helpers.py
+++ send-api/helpers/gpio_helpers.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
 
 def setupPins():
-    global L1, L2, L3, L4, C1, C2, C3, C4,LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7
     # Setup GPIO connectors for numberpad
     L1 = 4
     L2 = 17
@@ -65,3 +64,4 @@ def setupPins():
     # 15: LCD Backlight +5V
     # 16: LCD Backlight GND
     print("Finished setup of pins")
+    return L1, L2, L3, L4, C1, C2, C3, C4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7 
--- send-api/start.py
+++ send-api/start.py
@@ -5,7 +5,9 @@ import Adafruit_DHT
 from time import sleep
 
 # Setup Pins
-gpio_helpers.setupPins()
+L1, L2, L3, L4, C1, C2, C3, C4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7 = \
+   gpio_helpers.setupPins()
 
+print(L1, L2, L3, L4, C1, C2, C3, C4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7 )
 # Run test lcd
 lcd.setup_lcd()
