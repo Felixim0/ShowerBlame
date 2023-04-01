9645017975a2d43d68f3e2e80b2d81f44a21f672
Felixim0 <aldam.gatesfelix@gmail.com>
2023-04-01 17:02:28 +0100

    Fixed call

--- send-api/start.py
+++ send-api/start.py
@@ -110,9 +110,9 @@ def buttonCheck():
             elif showerRunning == True:
                 # Shower already running, we now want to cancell the shower
                 # Set the time to essentially nothing to allow other thread to handle it
-                stopShowerThread = threading.Thread(target=api.buttonCheck, args=())
+                stopShowerThread = threading.Thread(target=api.stopShower, args=())
                 stopShowerThread.start()
-                
+
                 setTime = "0:00"
                 sleep(2)
 
