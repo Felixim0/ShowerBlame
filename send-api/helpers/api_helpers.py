86b699494e579ca108eebcb6bd864081df51988e
Felixim0 <aldam.gatesfelix@gmail.com>
2023-04-01 15:55:17 +0100

    Added settime

--- send-api/helpers/api_helpers.py
+++ send-api/helpers/api_helpers.py
@@ -3,20 +3,26 @@ import threading
 
 
 def startTimer(**setTime):
-  print('starting the timer seperately')
-  print(setTime)
+  print()
+  print('TIMER start')
+  print('Set time value is:', str(setTime))
   time = setTime.get('setTime')
   # Send message to countdown timer
   try:
       response = requests.get('http://192.168.1.193:5000/startTimer/' + str(setTime) + '/01')
+      print('URL: ')
       print('http://192.168.1.193:5000/startTimer/' + str(setTime) + '/01')
+      print()
   except:
       print("Error starting the timer")
 
 def startShower(**setTime):
   time = setTime.get('setTime')
-  print('Sending START')
-  print(setTime)
+  print()
+  print('Start Shower (will now start timer thread and send disaply unit message)')
+  print('Set setTime value is:', str(setTime))
+  print('Set time value is:', str(time))
+  print()
   try:
     # Start new thread to start the countdown timer
     startTimerThread = threading.Thread(target=startTimer, kwargs={'setTime': setTime})
