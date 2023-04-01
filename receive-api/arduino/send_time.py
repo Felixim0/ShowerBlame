b4660ddecded8f0f62b113a71ebacecb365cc644
Felixim0 <aldam.gatesfelix@gmail.com>
2023-04-01 05:44:16 +0100

    The Last COmmit had working scripts on both sides! In this one those scripts are moved to backujp, and a 'reset' option has been added

--- receive-api/arduino/send_time.py
+++ receive-api/arduino/send_time.py
@@ -7,5 +7,10 @@ time.sleep(2)
 def send_start_time(minutes, seconds):
     ser.write(f"{minutes},{seconds}".encode())
 
+def reset_timer():
+    ser.write(b"reset")
+
 if __name__ == "__main__":
     send_start_time(10, 20)  # Send start time of 10 minutes and 20 seconds
+    time.sleep(5)  # Wait for 5 seconds
+    reset_timer()  # Reset the timer
